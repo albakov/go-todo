c9c1740bbab30b197f74468985934f8febde225e
ismail <ismail.albakov@gmail.com>
2022-04-09 13:14:44 +0300

    add completed field for item model

--- internal/app/model/Item.go
+++ internal/app/model/Item.go
@@ -1,6 +1,7 @@
 package model
 
 type Item struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed int    `json:"completed"`
 }

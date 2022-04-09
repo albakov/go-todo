b4cb04620988436e70dd1a17eed13c26e615390c
ismail <ismail.albakov@gmail.com>
2022-04-09 13:15:05 +0300

    fix cors

--- internal/app/server/apisever.go
+++ internal/app/server/apisever.go
@@ -11,14 +11,16 @@ import (
 )
 
 type apiserver struct {
-	router *mux.Router
-	store  store.Store
+	router      *mux.Router
+	store       store.Store
+	allowOrigin string
 }
 
 func apiServer() *apiserver {
 	s := &apiserver{
-		router: mux.NewRouter(),
-		store:  store.Store{},
+		router:      mux.NewRouter(),
+		store:       store.Store{},
+		allowOrigin: "http://js-todo.ru.local",
 	}
 	s.setRoutes()
 
@@ -29,21 +31,27 @@ func (s *apiserver) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
 }
 
+func (s *apiserver) setRoutes() {
+	s.router.Use(mux.CORSMethodMiddleware(s.router))
+
+	sub := s.router.PathPrefix("/api").Subrouter()
+	sub.HandleFunc("/items", s.getItems).Methods("GET", "OPTIONS")
+	sub.HandleFunc("/items", s.createItem).Methods("POST")
+	sub.HandleFunc("/items/{id:[0-9]+}", s.getItem).Methods("GET", "OPTIONS")
+	sub.HandleFunc("/items/{id:[0-9]+}", s.updateItem).Methods("POST")
+	sub.HandleFunc("/items/{id:[0-9]+}", s.deleteItem).Methods("DELETE", "OPTIONS")
+}
+
 func (s *apiserver) respond(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Access-Control-Allow-Origin", s.allowOrigin)
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST, DELETE")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if data != nil {
 		json.NewEncoder(w).Encode(data)
 	}
-}
-
-func (s *apiserver) setRoutes() {
-	sub := s.router.PathPrefix("/api").Subrouter()
-	sub.HandleFunc("/items", s.getItems).Methods("GET")
-	sub.HandleFunc("/items", s.createItem).Methods("POST")
-	sub.HandleFunc("/items/{id:[0-9]+}", s.getItem).Methods("GET")
-	sub.HandleFunc("/items/{id:[0-9]+}", s.updateItem).Methods("POST")
-	sub.HandleFunc("/items/{id:[0-9]+}", s.deleteItem).Methods("DELETE")
 }
 
 func (s *apiserver) getItems(w http.ResponseWriter, r *http.Request) {
@@ -54,19 +62,19 @@ func (s *apiserver) getItems(w http.ResponseWriter, r *http.Request) {
 func (s *apiserver) getItem(w http.ResponseWriter, r *http.Request) {
 	param, found := mux.Vars(r)["id"]
 	if !found {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	id, err := strconv.Atoi(param)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	item, err := s.store.Item().GetById(id)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
@@ -78,13 +86,13 @@ func (s *apiserver) createItem(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(data)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	item, err := s.store.Item().Create(*data)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
@@ -94,13 +102,13 @@ func (s *apiserver) createItem(w http.ResponseWriter, r *http.Request) {
 func (s *apiserver) updateItem(w http.ResponseWriter, r *http.Request) {
 	param, found := mux.Vars(r)["id"]
 	if !found {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	id, err := strconv.Atoi(param)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
@@ -108,13 +116,13 @@ func (s *apiserver) updateItem(w http.ResponseWriter, r *http.Request) {
 
 	err = json.NewDecoder(r.Body).Decode(data)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	err = s.store.Item().Update(id, *data)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
@@ -124,19 +132,19 @@ func (s *apiserver) updateItem(w http.ResponseWriter, r *http.Request) {
 func (s *apiserver) deleteItem(w http.ResponseWriter, r *http.Request) {
 	param, found := mux.Vars(r)["id"]
 	if !found {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	id, err := strconv.Atoi(param)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
 	err = s.store.Item().Delete(id)
 	if err != nil {
-		http.NotFound(w, r)
+		s.respond(w, http.StatusNotFound, nil)
 		return
 	}
 
